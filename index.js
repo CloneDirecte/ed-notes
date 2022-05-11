5a1cb725c5da2e0a8681fb230f67f8c1f069fe5b
Hugo <24863815+bgpvp@users.noreply.github.com>
2022-05-11 23:40:37 +0200

    feat: added login and notes functions

--- index.js
+++ index.js
@@ -34,3 +34,40 @@ export default async function edNotes(username, password) {
     }
   }
 }
+
+export async function login(username, password) {
+  if (typeof username !== "string" || typeof password !== "string") {
+    return "Nom d'utilisateur ou mot de passe n'est pas une chaîne de caractères ";
+  } else {
+    var raw = `data={\n	\"identifiant\": \"${username}\",\n	\"motdepasse\": \"${password}\"\n}`;
+    var config = {
+      method: "post",
+      url: "https://api.ecoledirecte.com/v3/login.awp",
+      headers: {},
+      data: raw,
+    };
+    const response = await axios(config);
+    if (response.data.code !== 200) {
+      return response.data.message;
+    } else {
+      return response.data;
+    }
+  }
+}
+
+export async function notes(token, accountId) {
+  var data = `data={\n\t"token": "${token}"\n}`;
+  var config = {
+    method: "post",
+    url: `https://api.ecoledirecte.com/v3/eleves/${accountId}/notes.awp?verbe=get`,
+    headers: {},
+    data: data,
+  };
+
+  const response = await axios(config);
+  if (response.data.code !== 200) {
+    return response.data.message;
+  } else {
+    return response.data;
+  }
+}
